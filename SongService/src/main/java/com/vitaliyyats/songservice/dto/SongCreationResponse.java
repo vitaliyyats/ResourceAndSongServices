89e93e593b2fa925fdbd7177fa3d733b7fd1a68c
Vitalii_Yatsunskyi <vitalii_yatsunskyi@epam.com>
2024-01-04 15:49:09 +0200

    initial commit

---
+++ SongService/src/main/java/com/vitaliyyats/songservice/dto/SongCreationResponse.java
@@ -0,0 +1,12 @@
+package com.vitaliyyats.songservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SongCreationResponse {
+    private Long id;
+}
