72b9b25fb37aa7f3954cf54c4182f1ac5b5ba0d2
Vitalii_Yatsunskyi <vitalii_yatsunskyi@epam.com>
2024-01-10 15:03:36 +0200

    fix

---
+++ EurekaServerApp/src/main/java/com/vitaliyyats/eurekaserverapp/EurekaServerAppApplication.java
@@ -0,0 +1,14 @@
+package com.vitaliyyats.eurekaserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerAppApplication.class, args);
+    }
+
+}
